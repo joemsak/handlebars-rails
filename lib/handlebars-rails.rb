4fa44df2ee84061c770a189af7e36d9b8b56e413
Daniel Roberts <danielrobertsdesign@gmail.com>
2013-02-13 17:19:05 -0500

    changes for infoactive

--- lib/handlebars-rails.rb
+++ lib/handlebars-rails.rb
@@ -10,19 +10,20 @@ module Handlebars::Rails
       config.handlebars = ActiveSupport::OrderedOptions.new
     end
     config.handlebars.override_ember_precompiler = false
-
+    
     initializer 'handlebars.handler.setup', :before => :add_view_paths do |app|
       app.paths['app/views'] << 'app/templates'
       ActiveSupport.on_load(:action_view) do
         ActionView::Template.register_template_handler(:hbs, ::Handlebars::TemplateHandler)
       end
     end
-
-    initializer 'handlebars.precompiler.setup', :group => :all, :after => 'ember_rails.setup' do |app|
-      if !defined?(Ember) || config.handlebars.override_ember_precompiler
-        app.assets.append_path 'app/templates'
-        app.assets.register_engine '.hbs', Handlebars::Tilt
-      end
+    
+    initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :all do |app|
+      next unless app.assets
+      app.assets.append_path 'app/templates'
+      app.assets.register_engine('.hbs', Tilt)
+      logger = Logger.new(STDOUT)
+      logger.debug("making link for assets registration engine");
     end
   end
 end
